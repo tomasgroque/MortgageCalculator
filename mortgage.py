9f07ac5156a4a2ea6d30bce2cb50c4a0d8cc829d
Tomy Guerrero <tomygrro@MacBook-Pro-de-Tomy.local>
2024-01-25 19:28:12 -0600

    realizando una prueba en el programa, calculo total mal realizado

--- mortgage.py
+++ mortgage.py
@@ -5,10 +5,10 @@ class Mortgage:
     interest_rate = 8.2
 
     def __init__(self, home_price, down_payment, loan_term = 10, interest_rate = 8.2):
-        self.home_price = home_price
-        self.down_payment = down_payment
-        self.loan_term = loan_term
-        self.interest_rate = interest_rate 
+        self.home_price = int(home_price)
+        self.down_payment = int(down_payment)
+        self.loan_term = int(loan_term)
+        self.interest_rate = float(interest_rate) 
     
     def mortgage_payment(self):
         loan_amount = self.home_price - self.down_payment
@@ -27,7 +27,18 @@ class Mortgage:
 calculating = True
 while calculating:
     print("Hello Costumer and Welcome to the Mortgage Calculator!!!")
-    home_price = input("Enter the cost of the home you wish to buy.")
-    down_payment = input("What's the down payment?")
-    loan_term = input("Enter the loan term in years.")
-    interest_rate = input("At what interest rate is your loan?")
+    home_price = input("Enter the cost of the home you wish to buy. ")
+    down_payment = input("What's the down payment? ")
+    loan_term = input("Enter the loan term in years. ")
+    interest_rate = input("At what interest rate is your loan? ")
+
+    new_mortgage = Mortgage(home_price, down_payment, loan_term, interest_rate)
+    total_payment = new_mortgage.mortgage_payment()
+    yearly = new_mortgage.yeraly_payment(total_payment)
+    monthly = new_mortgage.monthly_payment(total_payment)
+
+    print("Total amount to pay {}".format(total_payment))
+    print("Year Payment {}".format(yearly))
+    print("Monthly Payments {}".format(monthly))
+
+    calculating = False
